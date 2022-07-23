80a9d70521e294ad4b3fa5ef438add74d84ca897
777AMR <meyram.ramazanov@gmail.com>
2022-07-23 17:58:39 +0600

    Chapter 3.

---
+++ src/main/java/com/therecklers/sburrestdemo/SpringBootBookApplication.java
@@ -0,0 +1,15 @@
+package com.therecklers.sburrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class SpringBootBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/therecklers/sburrestdemo/SpringBootBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.therecklers.sburrestdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
