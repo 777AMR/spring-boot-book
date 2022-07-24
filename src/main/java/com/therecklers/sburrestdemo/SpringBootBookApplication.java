ff28d00dfc5f87eb697acefc7ed6fca412ffa792
777AMR <meyram.ramazanov@gmail.com>
2022-07-24 15:43:30 +0600

    Chapter 5.

--- src/main/java/com/therecklers/sburrestdemo/SpringBootBookApplication.java
+++ src/main/java/com/therecklers/sburrestdemo/SpringBootBookApplication.java
@@ -1,15 +1,23 @@
 package com.therecklers.sburrestdemo;
 
+import com.therecklers.sburrestdemo.entity.Droid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.UUID;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SpringBootBookApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootBookApplication.class, args);
     }
 
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    Droid createDroid() {
+        return new Droid();
+    }
 }
